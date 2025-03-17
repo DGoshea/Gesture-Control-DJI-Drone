b6cdade0fd847a4617e4b765e6b8b69f4d521f97
Divine Goshea <168388060+DGoshea@users.noreply.github.com>
2025-03-16 20:26:12 -0500

    Methods declared needs implementation

---
+++ camera_manager.py
@@ -0,0 +1,15 @@
+# camera_manager.py
+import cv2
+
+#Check open cv to imlement methods
+class CameraManager:
+    def __init__(self,):
+        
+    def setup(self):
+    
+    def get_frame(self):
+        
+    def adjust_settings(self):
+        
+    def release(self):
+        
