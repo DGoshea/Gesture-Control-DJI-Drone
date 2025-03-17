2402ce312ccabc724119f15668ae7c4a430d7e5e
Divine Goshea <168388060+DGoshea@users.noreply.github.com>
2025-03-16 20:24:03 -0500

    Methods declared needs implementation

---
+++ drone_controller.py
@@ -0,0 +1,17 @@
+# drone_controller.py
+from djitellopy import Tello
+import time
+
+#Check DJITello docs for method implementations 
+class DroneController:
+    def __init__(self, safety_bounds=True):
+    
+    def connect(self):
+    
+    def update_telemetry(self):
+    
+    def execute_command(self, command):
+    
+    def land(self):
+    
+    def disconnect(self):
